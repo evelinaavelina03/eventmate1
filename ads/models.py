86186222a0207f0830f181984c9849193496db94
evelinaavelina03 <evelinaaavelina@gmail.com>
2026-05-15 15:38:23 +0300

    Add events and ads models

--- ads/models.py
+++ ads/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Request(models.Model):
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    advertisement = models.ForeignKey('events.Event', on_delete=models.CASCADE)
+    comment = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        unique_together = ('user', 'advertisement')
+    
+    def __str__(self):
+        return f"Отклик от {self.user.username} на {self.advertisement.title}"
