2da1e23412c4523912c486e1da7afa3caa1a0888
evelinaavelina03 <evelinaaavelina@gmail.com>
2026-05-17 17:53:57 +0300

    Update events models

--- events/models.py
+++ events/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Status(models.Model):
+    STATUS_CHOICES = [
+        ('active', 'Активно'),
+        ('closed', 'Закрыто'),
+        ('moderation', 'На модерации'),
+    ]
+    name = models.CharField(max_length=50, choices=STATUS_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.get_name_display()
+
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE)
+    author = models.ForeignKey('users.Author', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
